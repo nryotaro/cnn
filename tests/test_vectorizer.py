968aff345ccba305171b772377f52a8d45d20529
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-08-22 00:35:05 +0900

    #1 wrote todo

--- tests/test_vectorizer.py
+++ tests/test_vectorizer.py
@@ -44,3 +44,7 @@ class TestVectorizer(object):
             np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
 
         assert (a == np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).all()
+
+    """
+    TODO Measure mean and std of padding data
+    """
