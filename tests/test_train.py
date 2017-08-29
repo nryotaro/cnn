f465808cd35ecd0f045a14bed6c8a99a21daacd9
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-08-29 13:12:02 +0900

    wrote test of binarize

---
+++ tests/test_train.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from hello_cnn.train import binarize
+from sklearn.preprocessing import LabelBinarizer
+
+
+def test_binarize():
+    labels = ('a', 'b', 'c')
+    bin = LabelBinarizer().fit(labels)
+    res = binarize(bin, labels)
+    assert (res == [[1, 0, 0], [0, 1, 0], [0, 0, 1]]).all()
